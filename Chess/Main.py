501f908bfe3fb60cd324d3958e7599bf38285e3a
arnepie <152080744+arnepie@users.noreply.github.com>
2024-02-29 20:35:31 +0100

    Update Main.py
    Adding piece movement by selecting squares

--- Chess/Main.py
+++ Chess/Main.py
@@ -5,7 +5,7 @@ import pygame
 game_state = Game_State.GameState()
 
 
-def draw_board():
+def draw_board(piece_selected):
     pygame.init()
 
     size = (1600, 1200)  # Width, Height
@@ -13,6 +13,7 @@ def draw_board():
 
     first_color = gray = 128, 128, 128  # RGB color code
     second_color = white = 255, 255, 255  # RGB color code
+    red = (255, 0, 0)
 
     screen = pygame.display.set_mode(size)  # Make screen
     pygame.display.set_caption("Chess")  # Set caption of screen
@@ -28,6 +29,15 @@ def draw_board():
             pygame.draw.rect(screen, first_color, pygame.Rect(pos_x, pos_y, 80, 80))
             pos_x += 80
             pygame.draw.rect(screen, second_color, pygame.Rect(pos_x, pos_y, 80, 80))
+
+    row_number = 0  # Starting row number
+    for row in game_state.board:
+        row_number += 1
+        square_number = 0  # Starting square number
+        for square in row:
+            square_number += 1
+            if square == piece_selected:
+                pygame.draw.rect(screen, red, pygame.Rect(400 + (80 * square_number), 100 + (80 * row_number), 80, 80))  # Making square of piece selected red
 
     pygame.display.flip()  # Updating screen
 
@@ -53,11 +63,65 @@ def draw_pieces(screen, board):
 def main():
     running = True
 
-    screen = draw_board()
-    draw_pieces(screen, game_state.board)
+    screen = draw_board(False)  # Drawing board
+    draw_pieces(screen, game_state.board)  # Drawing pieces
+    player_clicks = []  # Making empty list for player clicks
+    holding_a_piece = False  # Setting holding_a_piece to false
+    piece_holding = None  # Setting piece_holding to None
 
     while running:
         for event in pygame.event.get():  # Events from pygame
+            if event.type == pygame.MOUSEBUTTONDOWN:  # Clicking Event
+                mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()  # Getting mouse pos
+                player_clicks.append(pygame.mouse.get_pos())  # Adding mouse pos to player clicks list
+                if len(player_clicks) == 1:
+                    holding_a_piece = False
+
+                    for row in game_state.board:
+                        for square in row:
+                            if square != "---":
+                                piece_pos_y = 215 + (80 * game_state.board.index(row))  # Getting Y pos of piece
+                                piece_pos_x = 515 + (80 * row.index(square))  # Getting X pos of piece
+
+                                if piece_pos_x - 40 < mouse_pos_x < piece_pos_x + 40 and piece_pos_y - 40 < mouse_pos_y < piece_pos_y + 40:  # Checking if mouse pos is on square of piece
+                                    piece_holding = square  # Setting piece_holding to the piece that was selected
+                                    holding_a_piece = True  # Setting holding_a_piece to true
+
+                    if not holding_a_piece:
+                        player_clicks = []  # If player is not holding a piece, the clicks get reset
+
+                if len(player_clicks) == 2:
+                    mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()  # Getting mouse pos
+                    row_number = -1  # Starting row number
+
+                    for row1 in game_state.board:
+                        row_number += 1
+                        square_number = -1  # Starting square number
+                        for _ in row1:
+                            square_number += 1
+                            square_pos_y = 215 + (80 * row_number)
+                            square_pos_x = 515 + (80 * square_number)
+
+                            if square_pos_x - 40 < mouse_pos_x < square_pos_x + 40 and square_pos_y - 40 < mouse_pos_y < square_pos_y + 40:  # Checking on which square mouse pos is
+                                row_set_index = row_number  # Defining row index of square selected
+                                square_set_index = square_number  # Defining square index of square selected
+
+                                if holding_a_piece:
+                                    for find_row in game_state.board:
+                                        for find_square in find_row:
+                                            if find_square == piece_holding:
+                                                find_row[find_row.index(piece_holding)] = "---"  # Setting original square to empty
+                                                set_row = game_state.board[row_set_index]  # Defining row
+                                                set_row[square_set_index] = piece_holding  # Changing square selected to piece selected
+
+                    player_clicks = []  # resetting player clicks
+                    holding_a_piece = False
+                    screen = draw_board(piece_holding)  # Drawing screen
+                    draw_pieces(screen, game_state.board)  # Drawing pieces
+
+                screen = draw_board(piece_holding)  # Drawing screen
+                draw_pieces(screen, game_state.board)  # Drawing pieces
+
             if event.type == pygame.QUIT:  # Quit event
                 running = False
 
