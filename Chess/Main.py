8250100739ade7a23ee21c34df6791f27c70c10b
arnepie <152080744+arnepie@users.noreply.github.com>
2024-03-01 13:28:10 +0100

    Adding white to move or black to move

--- Chess/Main.py
+++ Chess/Main.py
@@ -1,6 +1,7 @@
 import pygame.image
 import Game_State
 import pygame
+import copy
 
 game_state = Game_State.GameState()
 
@@ -59,6 +60,8 @@ def draw_pieces(screen, board):
 
     pygame.display.flip()  # Updating screen
 
+    print("drawn")
+
 
 def main():
     running = True
@@ -79,7 +82,14 @@ def main():
 
                     for row in game_state.board:
                         for square in row:
-                            if square != "---":
+                            if game_state.white_to_move and square[:1] == "w":
+                                piece_pos_y = 215 + (80 * game_state.board.index(row))  # Getting Y pos of piece
+                                piece_pos_x = 515 + (80 * row.index(square))  # Getting X pos of piece
+
+                                if piece_pos_x - 40 < mouse_pos_x < piece_pos_x + 40 and piece_pos_y - 40 < mouse_pos_y < piece_pos_y + 40:  # Checking if mouse pos is on square of piece
+                                    piece_holding = square  # Setting piece_holding to the piece that was selected
+                                    holding_a_piece = True  # Setting holding_a_piece to true
+                            if not game_state.white_to_move and square[:1] == "b":
                                 piece_pos_y = 215 + (80 * game_state.board.index(row))  # Getting Y pos of piece
                                 piece_pos_x = 515 + (80 * row.index(square))  # Getting X pos of piece
 
@@ -89,11 +99,14 @@ def main():
 
                     if not holding_a_piece:
                         player_clicks = []  # If player is not holding a piece, the clicks get reset
+                    else:
+                        screen = draw_board(piece_holding)  # Drawing screen
+                        draw_pieces(screen, game_state.board)  # Drawing pieces
 
                 if len(player_clicks) == 2:
                     mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()  # Getting mouse pos
-                    row_number = -1  # Starting row number
 
+                    row_number = -1  # Starting row number
                     for row1 in game_state.board:
                         row_number += 1
                         square_number = -1  # Starting square number
@@ -114,13 +127,38 @@ def main():
                                                 set_row = game_state.board[row_set_index]  # Defining row
                                                 set_row[square_set_index] = piece_holding  # Changing square selected to piece selected
 
+                                                if len(game_state.moves) < 1:
+                                                    game_state.moves.append(copy.deepcopy(game_state.board))  # Making deepcopy of board and adding it to the moves list
+                                                    game_state.white_to_move = not game_state.white_to_move  # Switching color to move each turn
+
+                                                elif game_state.board != game_state.moves[-1]:
+                                                    game_state.moves.append(copy.deepcopy(game_state.board))  # Making deepcopy of board and adding it to the moves list
+                                                    game_state.white_to_move = not game_state.white_to_move  # Switching color to move each turn
+
                     player_clicks = []  # resetting player clicks
                     holding_a_piece = False
-                    screen = draw_board(piece_holding)  # Drawing screen
+                    screen = draw_board(None)  # Drawing screen
                     draw_pieces(screen, game_state.board)  # Drawing pieces
+            if event.type == pygame.KEYDOWN:  # Key down event
+                if event.key == pygame.K_BACKSPACE:  # Backspace event
+                    if len(game_state.moves) > 1:
+                        game_state.board = game_state.moves[-2]  # Board is set to second last move
+                        game_state.moves.pop(-1)  # Last move is removed from moves list
+                    else:  # If only 1 move is played, board is reset and moves list is also reset
+                        game_state.board = [
+                                            ["bR1", "bN1", "bB1", "bQ", "bK", "bB2", "bN2", "bR2"],
+                                            ["bp1", "bp2", "bp3", "bp4", "bp5", "bp6", "bp7", "bp8"],
+                                            ["---", "---", "---", "---", "---", "---", "---", "---"],
+                                            ["---", "---", "---", "---", "---", "---", "---", "---"],
+                                            ["---", "---", "---", "---", "---", "---", "---", "---"],
+                                            ["---", "---", "---", "---", "---", "---", "---", "---"],
+                                            ["wp1", "wp2", "wp3", "wp4", "wp5", "wp6", "wp7", "wp8"],
+                                            ["wR1", "wN1", "wB1", "wQ", "wK", "wB2", "wN2", "wR2"]]
+                        game_state.moves = []
 
-                screen = draw_board(piece_holding)  # Drawing screen
-                draw_pieces(screen, game_state.board)  # Drawing pieces
+                    screen = draw_board(None)  # Drawing screen
+                    draw_pieces(screen, game_state.board)  # Drawing pieces
+                    game_state.white_to_move = not game_state.white_to_move  # Switching color to move each turn
 
             if event.type == pygame.QUIT:  # Quit event
                 running = False
